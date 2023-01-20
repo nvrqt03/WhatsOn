e67d4ce5a56f9550dbafdbf93c85fd2c2b1ed2cd
aj mitchell <ajktg21@gmail.com>
2023-01-20 11:50:36 -0600

    initial commit

---
+++ app/src/main/java/com/example/whatson/MainActivity.kt
@@ -0,0 +1,13 @@
+import com.example.whatson.R
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
